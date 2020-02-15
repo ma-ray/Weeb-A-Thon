002f36c94f81fb466ae0650da219e52762febdb8
rayma01 <60418739+rayma01@users.noreply.github.com>
2020-02-15 04:59:13 -0500

    Add files via upload

--- WebScrape.py
+++ WebScrape.py
@@ -23,6 +23,12 @@ class a:
         self.score=sc
 
 
+def removeNewline(string):
+    string=string.split('\n')
+    s=''
+    for i in string:
+        s+=i.strip()
+    return s
 
 
 def profileLoad(user = "EpicSteve2"):
@@ -33,10 +39,10 @@ def main():
     animeSoup = BeautifulSoup( get("https://myanimelist.net/anime/season").text, "html.parser")
     animeList = animeSoup.findAll('div', class_="seasonal-anime js-seasonal-anime")
     animes=[]
-    for anime in animeList[:1]:
+    for anime in animeList:
         animes.append(a(
         (anime.find('div',class_="title").text.split('\n')[1]),#title
-        (anime.find('div',class_="synopsis js-synopsis").text.strip()),#syn
+        removeNewline(anime.find('div',class_="synopsis js-synopsis").text.strip()),#syn
         ((anime.find('div', class_="genres js-genre").find('div',class_="genres-inner js-genre-inner")).text.strip().split('\n\n')),#gen
         (anime.find('div',class_="eps").text.split('\n\n')[0].strip()),#eps
         (anime.find('div', class_="prodsrc").text.split('\n\n')[0].strip()),  # producer
@@ -44,6 +50,26 @@ def main():
         (anime.find('div', class_="information").find('span', class_= "member fl-r").text.strip()) , # mem
         (anime.find('span', class_="score").text.strip())  # type
         ))
+    
+        
+    fileWrite = open("test.txt", "w")
+        
+    for current in animes[:10]:
+        fileWrite.writelines(current.title + "\n")
+        fileWrite.writelines(current.synopsis + "\n")
+        
+        for genre in current.genre:
+            fileWrite.write(genre + ",")
+        fileWrite.write("\n")
+        
+        fileWrite.writelines(current.numOfEpisodes + "\n")
+        fileWrite.writelines(current.producer + "\n")
+        fileWrite.writelines(current.type + "\n")
+        fileWrite.writelines(current.numOfMembers + "\n")
+        fileWrite.writelines(current.score + "\n\n")
+        
+    fileWrite.close()
+    print("Item has been written!")
 
 if __name__ == "__main__":
     main()
