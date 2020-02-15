9b92b3125eaf55d62c5c85e22097619ea6588676
Epicsteve2 <51217386+Epicsteve2@users.noreply.github.com>
2020-02-15 03:23:30 -0500

    Add files via upload

--- Utils.java
+++ Utils.java
@@ -3,4 +3,17 @@ public class Utils {
     public static double getScore (int numRelevantTags, int numDislikedTags) {
         return min(100, max(0, max(10 * numRelevantTags, 100) + max(-10 * numDislikedTags, -100)));
     }
+
+    public void printList(Anime[] animeList) {
+        int length = length(animeList);
+        for (int i = 0; i < length; i++) {
+            System.out.println("Title: " + animeList[i].title);
+            System.out.println("Score: " + animeList[i].score);
+            System.out.println("Description: " + animeList[i].description);
+            System.out.println("Type: " + animeList[i].type);
+            System.out.println("AkScore: " + animeList[i].AkScore);
+            System.out.println("Genres: " + animeList[i].genres);
+            System.out.println("\n");
+        }
+    }
 }
