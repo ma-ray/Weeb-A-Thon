238ab8c294753cae2a901412b5d2e408833a9d61
Khalix01 <61076606+Khalix01@users.noreply.github.com>
2020-02-15 03:31:15 -0500

    Add files via upload
    Web scrape

---
+++ WebScrape.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+
+class anime:
+    title=""
+    synopsis=""
+    genre=[]
+    numOfEpisodes=""
+    producer=""
+    type=""
+    numOfMembers=""
+    score=""
+
+    def _init_(self, title, syn, gen, ep, pro, type, mem, sc):
+        self.title=title
+        self.synopsis=syn
+        self.genre=gen
+        self.numOfEpisodes=ep
+        self.producer=pro
+        self.type=type
+        self.numOfMembers=mem
+        self.score-sc
+
+
+
+
+def profileLoad(user = "EpicSteve2"):
+    profileSoup = BeautifulSoup(get("https://myanimelist.net/profile/" + user).text, "html.parser")
+
+
+def main():
+    animeSoup = BeautifulSoup( get("https://myanimelist.net/anime/season").text, "html.parser")
+    animeList = animeSoup.findAll('div', class_="seasonal-anime js-seasonal-anime")
+    animes=[]
+    for anime in animeList[:1]:
+        animes.append(anime(
+            (anime.find('div',class_="title").text.split('\n')[1]),#title
+            (anime.find('div',class_="synopsis js-synopsis").text.strip()),#syn
+            ((anime.find('div', class_="genres js-genre").find('div',class_="genres-inner js-genre-inner")).text.strip().split('\n\n')),#gen
+            (anime.find('div',class_="eps").text.split('\n\n')[0].strip()),#eps
+            (anime.find('div', class_="prodsrc").text.split('\n\n')[0].strip()),  # producer
+            (anime.find('span',class_="source").text) , # type
+            (anime.find('div', class_="information").find('span', class_= "member fl-r").text.strip()) , # mem
+            (anime.find('span', class_="score").text.strip())  # type
+        ))
+
+if __name__ == "__main__":
+    main()
